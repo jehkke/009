0978c24209d9f2c70ac536acbb41f5ec4265dcee
jehkke <jbndoit@gmail.com>
2023-11-05 22:04:21 +0900

    실습 3-2

---
+++ 실습3-2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N_STUDENT  5
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+int main(int argc, char *argv[]) {
+	
+	int i;
+	int a[N_STUDENT]={1,2,3,4,5};
+	int b[N_STUDENT]={1,2,3,4,5};
+	
+	if(a==b)
+	printf("array a and b are the same\n");
+	else
+		printf("array a and b are not the same");
+	system("PAUSE");
+	return 0;
+	
+	THIS IS WRONG CODING!!
+}
+*/
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	int a[N_STUDENT]={1,2,3,4,5};
+	int b[N_STUDENT]={1,2,3,4,5};
+	int flag = 0;
+	
+	for (i=0;i<N_STUDENT;i++)
+	{
+		if(a[i] != b[i])
+		{
+			printf("array a and b are not the same\n");
+			flag=1; //if wrong, flag=0 --> flag=1  
+		}
+	}
+	if (flag==0) // if flag is still 0, printf array a and b are the same
+		printf("array a and b are the same ");
+		
+	
+	system("PAUSE");
+	return 0;
+	
+}
+
+
+
