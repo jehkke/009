fb3624eb0b53d2570f1fab866107f83a39628c80
jehkke <jbndoit@gmail.com>
2023-11-05 21:36:25 +0900

    실습 2-2

---
+++ 실습2-2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N_STUDENT  5
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char *argv[]) {
+	
+	int i, average;
+	int sum;
+	int grade[N_STUDENT];
+	printf("input 5 score:");
+	
+	sum=0;
+	for (i=0;i<N_STUDENT;i++)
+	{
+		scanf("%d", &grade[i]);
+		sum+=grade[i];
+	}
+	
+	average = sum/ N_STUDENT;
+	printf("score average: %i\n",average);
+	
+	system("PAUSE");
+	return 0;
+	
+}
+
+
